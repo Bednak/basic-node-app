d6ec7cd23afadd8d47c0d29ae49d56453d980e16
Kamil <kamilbednarz91@gmail.com>
2019-06-30 11:01:56 -0500

    Basic Node App

---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require("./greeting.js");
+const goodbyes= require("./goodbye.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+goodbyes.goodbyeEnglish();
+
+goodbyes.goodbyeJapanese();
+
+goodbyes.goodbyeSpanish();
+
+goodbyes.goodbyeIcelandic();
